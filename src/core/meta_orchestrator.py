8e6aa190a1d011d443d8eb4ea751a655e14fe5a4
NightSpirit2099 <98102729+NightSpirit2099@users.noreply.github.com>
2025-08-03 10:46:09 -0300

    feat: add execute orchestration method

--- src/core/meta_orchestrator.py
+++ src/core/meta_orchestrator.py
@@ -1,4 +1,8 @@
-from .interfaces import IExecutionStrategy, UserRequest
+import logging
+
+from .interfaces import AgentResponse, IExecutionStrategy, UserRequest
+
+logger = logging.getLogger(__name__)
 
 
 class MetaOrchestrator:
@@ -11,3 +15,24 @@ class MetaOrchestrator:
     def select_strategy(self, analysis) -> IExecutionStrategy:
         """Seleciona a estratégia apropriada com base na análise."""
         raise NotImplementedError
+
+    def execute(self, request: UserRequest) -> AgentResponse:
+        """Processa a requisição do usuário e retorna a resposta do agente."""
+        logger.info("Iniciando processamento da requisição do usuário")
+
+        logger.debug("Analisando a requisição")
+        analysis = self.analyze_request(request)
+        logger.debug("Análise concluída: %s", analysis)
+
+        logger.debug("Selecionando estratégia")
+        strategy = self.select_strategy(analysis)
+        logger.debug(
+            "Estratégia selecionada: %s", strategy.__class__.__name__
+        )
+
+        logger.debug("Executando estratégia")
+        response = strategy.execute(request)
+        logger.debug("Execução concluída: %s", response)
+
+        logger.info("Processamento da requisição concluído")
+        return response
